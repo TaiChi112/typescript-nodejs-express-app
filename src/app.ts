1ad6a43b95fa2927a4e21ad389b5f8fe338ec819
TaiChi <anothai.0978452316@gmail.com>
2024-09-15 16:11:03 +0700

    delete : node_modules before push on gh

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import UserController from './controllers/UserController';
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.json());
 
